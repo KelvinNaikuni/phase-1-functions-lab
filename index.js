ad15a41cb8bc4bc07b6b50e974d31dcd45bc2323
KelvinNaikuni <kelvin.naikuni@student.moringaschool.com>
2023-09-21 20:52:07 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (distance) {
+if (distance===43)return 1;
+else if (distance===50) return 8;
+else if (distance===34) return 8;
+}
+function distanceFromHqInFeet (feet) {
+if (feet===43){ 
+  return 264;
+}
+if (feet===50) {
+  return 2112;
+}
+if (feet===34) {
+   return 2112;
+}
+}
+function distanceTravelledInFeet (total) {
+  if (total==43&&48) {
+  return 1320;
+  }
+if (total==50&&60) {
+  return 2640;
+}
+if (total==34&&38) {
+   return 1584;
+}}
+
+
+function calculatesFarePrice(start, destination) {
+  const feetTraveled = Math.abs(destination - start) * 264; // Each block is 264 feet
+
+  if (feetTraveled <= 400) {
+    return 0;
+  } else if (feetTraveled <= 2000) {
+    const extraFeet = feetTraveled - 400;
+    const fareCents = extraFeet * 0.02; // 2 cents per foot
+    return fareCents;
+  } else if (feetTraveled <= 2500) {
+    return 25; // Flat rate for over 2000 feet
+  } else {
+    return 'cannot travel that far';
+  }}
--- test/helpers.js
+++ test/helpers.js
@@ -18,3 +18,21 @@ const src = babelResult.code
 jsdom({
   html, src
 });
+
+
+function calculatesFarePrice (start, destination) {
+  let x = distanceFromHqInFeet(start, destination)
+if (x<=400){
+  return 0;
+}
+else if (x>400 & x<2000){
+  let y=x-400
+  let z =  y * 0.02;
+  return z
+}
+else if (x>2000 & x<=2500 ){
+  return 25;
+}
+ else {
+ return 'cannot travel that far';}
+}
